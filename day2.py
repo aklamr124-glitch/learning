b2f54a94e440d710d6fabd0bf3203deaaafb8ddd
Amr Akl <aklamr124@gmail.com>
2025-08-22 20:45:35 +0300

    day2 more variable

--- day2.py
+++ day2.py
@@ -1,4 +1,9 @@
 name = input ("enter your name ") 
 age = int (input ("enter your age "))
+height = float(input ("enter your height"))
+
+is_student = True
 print ("your name is  :" , name , "    your age is : " , str( age))
 print (f"your age after 10 years  {age+10} ")
+print ("your height is :", height)
+print ("Are you student " , is_student)
